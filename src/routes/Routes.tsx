82fc2b93559f55762dc4e7de1cc709467073d89e
Oreki13 <arfandynugraha21@gmail.com>
2020-03-17 15:38:22 +0700

    update react navigation v5

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
 import {Button, View, Text} from 'react-native';
-// import {createAppContainer, createSwitchNavigator} from 'react-navigation';
-// import {createStackNavigator} from 'react-navigation-stack';
-// import {createDrawerNavigator} from 'react-navigation-drawer';
+
 import Home from '../screen/Home';
 import styleDrawer from './styleDrawer';
 import Chat from '../screen/chat';
@@ -33,36 +31,6 @@ const Drawer = createDrawerNavigator<DrawerParamList>();
 const StackHome = createStackNavigator<HomeParamList>();
 const SideBar = createStackNavigator<SideBarList>();
 
-// const AuthPage = () => {
-//   return (
-//     <Stack.Navigator
-//       initialRouteName="Auth"
-//       screenOptions={{
-//         header: () => null,
-//       }}>
-//       <Stack.Screen name="Auth" component={Auth} />
-//       <Stack.Screen name="Login" component={Login} />
-//       <Stack.Screen name="SignUp" component={SignUp} />
-//     </Stack.Navigator>
-//   );
-// };
-// export const HomePage = () => {
-//   console.log('asd');
-
-//   return (
-//     <Stack.Navigator
-//       screenOptions={{
-//         header: () => null,
-//       }}>
-//       <Stack.Screen name="Home" component={() => <Home />} />
-//       {/* <Stack.Screen name="Profile" component={Profile} />
-//       <Stack.Screen name="Chat" component={Chat} />
-//       <Stack.Screen name="Maps" component={Maps} />
-//       <Stack.Screen name="ProfileFriends" component={ProfileFriends} /> */}
-//     </Stack.Navigator>
-//   );
-// };
-
 export const HomeStack: React.FC<HomeStack> = ({}) => {
   return (
     <StackHome.Navigator
@@ -76,27 +44,12 @@ export const HomeStack: React.FC<HomeStack> = ({}) => {
     </StackHome.Navigator>
   );
 };
-// export const SideBar: React.FC<SideBar> = ({}) => {
-//   return (
-//     <SideBar.Navigator
-//       initialRouteName="Home"
-//       screenOptions={{
-//         header: () => null,
-//       }}>
-//       <SideBar.Screen name="Home" component={Home} />
-//       <SideBar.Screen name="Profile" component={Profile} />
-//       {/* <SideBar.Screen name="ProfileFriends" component={ProfileFriends} /> */}
-//     </SideBar.Navigator>
-//   );
-// };
 
 export const PageDrawer: React.FC<Drawer> = ({}) => {
   return (
     <Drawer.Navigator
       initialRouteName="HomePage"
       drawerPosition="left"
-      // drawerContent={styleDrawer}
-
       drawerType="front">
       <Drawer.Screen name="HomePage" component={HomeStack} />
       <Drawer.Screen name="MapsPage" component={Maps} />
@@ -122,63 +75,3 @@ export const Auths: React.FC<Routes> = ({}) => {
     </NavigationContainer>
   );
 };
-
-// const AuthStack = createStackNavigator(
-//   {
-//     Login,
-//     SignUp,
-//   },
-//   {
-//     initialRouteName: 'Login',
-//     headerMode: 'none',
-//   },
-// );
-
-// const HomeStack = createStackNavigator(
-//   {
-//     Home,
-//     Profile,
-//     Chat,
-//     Maps,
-//     ProfileFriends,
-//   },
-//   {
-//     initialRouteName: 'Home',
-
-//     headerMode: 'none',
-//   },
-// );
-
-// const stackDrawer = createDrawerNavigator(
-//   {
-//     HomeStack,
-//   },
-//   {
-//     initialRouteName: 'HomeStack',
-//     drawerPosition: 'left',
-//     contentComponent: styleDrawer,
-//     drawerType: 'front',
-//   },
-// );
-
-// const Router = createSwitchNavigator(
-//   {
-//     AuthStack,
-//     stackDrawer,
-//     Auth,
-//   },
-//   {
-//     initialRouteName: 'Auth',
-//     headerMode: 'none',
-//   },
-// );
-
-// function Navigation() {
-//   return (
-//     <NavigationContainer>
-//       <PageDrawer />
-//     </NavigationContainer>
-//   );
-// }
-
-// export default Navigation;
